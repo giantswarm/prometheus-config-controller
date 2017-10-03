beccdc024ebbbfa39c028daed06c3df4f90d4528
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-10-03 15:02:21 +0100

    Adds no-op delete processing

--- service/resource/configmap/delete_test.go
+++ service/resource/configmap/delete_test.go
@@ -1,1 +1,59 @@
 package configmap
+
+import (
+	"context"
+	"testing"
+
+	"github.com/giantswarm/micrologger/microloggertest"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+// Test_Resource_ConfigMap_GetDeleteState tests the GetDeleteState method.
+func Test_Resource_ConfigMap_GetDeleteState(t *testing.T) {
+	fakeK8sClient := fake.NewSimpleClientset()
+
+	resourceConfig := DefaultConfig()
+
+	resourceConfig.K8sClient = fakeK8sClient
+	resourceConfig.Logger = microloggertest.New()
+
+	resourceConfig.ConfigMapName = "prometheus"
+	resourceConfig.ConfigMapNamespace = "monitoring"
+
+	resource, err := New(resourceConfig)
+	if err != nil {
+		t.Fatalf("error returned creating resource: %s\n", err)
+	}
+
+	deleteState, err := resource.GetDeleteState(context.TODO(), v1.Service{}, v1.ConfigMap{}, v1.ConfigMap{})
+	if err != nil {
+		t.Fatalf("error returned getting delete state: %s\n", err)
+	}
+
+	if deleteState != nil {
+		t.Fatalf("delete state should be nil, was: %#v", deleteState)
+	}
+}
+
+// Test_Resource_ConfigMap_ProcessDeleteState tests the ProcessDeleteState method.
+func Test_Resource_ConfigMap_ProcessDeleteState(t *testing.T) {
+	fakeK8sClient := fake.NewSimpleClientset()
+
+	resourceConfig := DefaultConfig()
+
+	resourceConfig.K8sClient = fakeK8sClient
+	resourceConfig.Logger = microloggertest.New()
+
+	resourceConfig.ConfigMapName = "prometheus"
+	resourceConfig.ConfigMapNamespace = "monitoring"
+
+	resource, err := New(resourceConfig)
+	if err != nil {
+		t.Fatalf("error returned creating resource: %s\n", err)
+	}
+
+	if err := resource.ProcessDeleteState(context.TODO(), v1.Service{}, v1.ConfigMap{}); err != nil {
+		t.Fatalf("error returned processing delete state: %s\n", err)
+	}
+}
