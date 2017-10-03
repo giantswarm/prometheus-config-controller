afebcc02166ca245b2497e3ca574fe5179212b3f
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-10-03 15:39:53 +0100

    Adds no-op delete processing (#9)

--- service/resource/configmap/delete.go
+++ service/resource/configmap/delete.go
@@ -4,10 +4,13 @@ import (
 	"context"
 )
 
+// GetDeleteState is a no-op.
+// We do not want to delete the configmap, as the running prometheus relies on it.
 func (r *Resource) GetDeleteState(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
 	return nil, nil
 }
 
+// ProcessDeleteState is a no-op.
 func (r *Resource) ProcessDeleteState(ctx context.Context, obj, deleteState interface{}) error {
 	return nil
 }
