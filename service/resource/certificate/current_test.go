2fca587639b3c03303e87d1d01a7e5b35f41d8b0
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-10-12 16:55:54 +0100

    Adds boilerplate for certificate resource (#13)

---
+++ service/resource/certificate/current_test.go
@@ -0,0 +1,9 @@
+package certificate
+
+import (
+	"testing"
+)
+
+// Test_Resource_Certificate_GetCurrentState tests the GetCurrentState method.
+func Test_Resource_Certificate_GetCurrentState(t *testing.T) {
+}
---
+++ service/resource/certificate/update_test.go
@@ -0,0 +1,13 @@
+package certificate
+
+import (
+	"testing"
+)
+
+// Test_Resource_Certificate_GetUpdateState tests the GetUpdateState method.
+func Test_Resource_Certificate_GetUpdateState(t *testing.T) {
+}
+
+// Test_Resource_Certificate_ProcessUpdateState tests the ProcessUpdateState method.
+func Test_Resource_Certificate_ProcessUpdateState(t *testing.T) {
+}
