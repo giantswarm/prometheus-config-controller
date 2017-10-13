3b3c0aaff6adc070cf926cccfc4dcf0ab3d284f7
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-10-13 16:07:41 +0100

    Adds certificate resource current state support

--- service/resource/certificate/certificate.go
+++ service/resource/certificate/certificate.go
@@ -1,23 +1,57 @@
 package certificate
 
 import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/framework"
+	"github.com/spf13/afero"
 )
 
 const (
 	Name = "certificate"
 )
 
-type Config struct{}
+type Config struct {
+	Fs     afero.Fs
+	Logger micrologger.Logger
 
-func DefaultConfig() Config {
-	return Config{}
+	CertificateDirectory string
 }
 
-type Resource struct{}
+func DefaultConfig() Config {
+	return Config{
+		Fs:     nil,
+		Logger: nil,
+
+		CertificateDirectory: "",
+	}
+}
+
+type Resource struct {
+	fs     afero.Fs
+	logger micrologger.Logger
+
+	certificateDirectory string
+}
 
 func New(config Config) (*Resource, error) {
-	resource := &Resource{}
+	if config.Fs == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Fs must not be empty")
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+
+	if config.CertificateDirectory == "" {
+		return nil, microerror.Maskf(invalidConfigError, "config.CertificateDirectory must not be empty")
+	}
+
+	resource := &Resource{
+		fs:     config.Fs,
+		logger: config.Logger,
+
+		certificateDirectory: config.CertificateDirectory,
+	}
 
 	return resource, nil
 }
@@ -28,4 +62,12 @@ func (r *Resource) Name() string {
 
 func (r *Resource) Underlying() framework.Resource {
 	return r
+}
+
+// certificateFile represents a PKI certificate on disk.
+type certificateFile struct {
+	// path is the location of the certificate on the filesystem.
+	path string
+	// data is the contents of the certificate.
+	data string
 }
--- service/resource/certificate/error.go
+++ service/resource/certificate/error.go
@@ -1,1 +1,12 @@
 package certificate
+
+import (
+	"github.com/giantswarm/microerror"
+)
+
+var invalidConfigError = microerror.New("invalid config")
+
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return microerror.Cause(err) == invalidConfigError
+}
