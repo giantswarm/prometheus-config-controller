38a45bc4a8b40ad6658b61319299914b574c63bb
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-11-01 10:39:24 +0000

    Adds wait before reloading to allow for configmap propagation (#38)

--- service/resource/configmap/create_test.go
+++ service/resource/configmap/create_test.go
@@ -3,6 +3,7 @@ package configmap
 import (
 	"context"
 	"testing"
+	"time"
 
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/pkg/api/v1"
@@ -26,6 +27,7 @@ func Test_Resource_ConfigMap_GetCreateState(t *testing.T) {
 	resourceConfig.ConfigMapKey = "prometheus.yml"
 	resourceConfig.ConfigMapName = "prometheus"
 	resourceConfig.ConfigMapNamespace = "monitoring"
+	resourceConfig.ReloadWaitTime = 1 * time.Second
 
 	resource, err := New(resourceConfig)
 	if err != nil {
@@ -56,6 +58,7 @@ func Test_Resource_ConfigMap_ProcessCreateState(t *testing.T) {
 	resourceConfig.ConfigMapKey = "prometheus.yml"
 	resourceConfig.ConfigMapName = "prometheus"
 	resourceConfig.ConfigMapNamespace = "monitoring"
+	resourceConfig.ReloadWaitTime = 1 * time.Second
 
 	resource, err := New(resourceConfig)
 	if err != nil {
--- service/resource/configmap/delete_test.go
+++ service/resource/configmap/delete_test.go
@@ -3,6 +3,7 @@ package configmap
 import (
 	"context"
 	"testing"
+	"time"
 
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/pkg/api/v1"
@@ -25,6 +26,7 @@ func Test_Resource_ConfigMap_GetDeleteState(t *testing.T) {
 	resourceConfig.ConfigMapKey = "prometheus.yml"
 	resourceConfig.ConfigMapName = "prometheus"
 	resourceConfig.ConfigMapNamespace = "monitoring"
+	resourceConfig.ReloadWaitTime = 1 * time.Second
 
 	resource, err := New(resourceConfig)
 	if err != nil {
@@ -55,6 +57,7 @@ func Test_Resource_ConfigMap_ProcessDeleteState(t *testing.T) {
 	resourceConfig.ConfigMapKey = "prometheus.yml"
 	resourceConfig.ConfigMapName = "prometheus"
 	resourceConfig.ConfigMapNamespace = "monitoring"
+	resourceConfig.ReloadWaitTime = 1 * time.Second
 
 	resource, err := New(resourceConfig)
 	if err != nil {
