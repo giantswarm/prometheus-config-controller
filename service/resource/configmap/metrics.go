82f019add8b9b90fc0e27017cdf934f4c1d72d3d
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-11-03 13:39:44 +0000

    Adds metric for number of scrape configs

--- service/resource/configmap/metrics.go
+++ service/resource/configmap/metrics.go
@@ -18,8 +18,18 @@ var (
 			Help:      "Size of the prometheus configmap.",
 		},
 	)
+
+	scrapeConfigCount = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "scrape_config_count",
+			Help:      "Count of scrape configs computed.",
+		},
+	)
 )
 
 func init() {
 	prometheus.MustRegister(configmapSize)
+	prometheus.MustRegister(scrapeConfigCount)
 }
