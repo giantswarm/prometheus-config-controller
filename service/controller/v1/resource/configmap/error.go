e622018ed1957a62efe5f82278500ebe4fa877c4
Tim Schindler <xh3b4sd@gmail.com>
2018-08-01 09:57:09 +0200

    rewrite errors (#86)

--- service/controller/v1/resource/configmap/error.go
+++ service/controller/v1/resource/configmap/error.go
@@ -4,49 +4,63 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var configMapNotFoundError = microerror.New("configmap not found")
+var configMapNotFoundError = &microerror.Error{
+	Kind: "configMapNotFoundError",
+}
 
 // IsConfigMapNotFound asserts configMapNotFoundError.
 func IsConfigMapNotFound(err error) bool {
 	return microerror.Cause(err) == configMapNotFoundError
 }
 
-var configMapKeyNotFoundError = microerror.New("configmap key not found")
+var configMapKeyNotFoundError = &microerror.Error{
+	Kind: "configMapKeyNotFoundError",
+}
 
 // IsConfigMapKeyNotFound asserts configMapKeyNotFoundError.
 func IsConfigMapKeyNotFound(err error) bool {
 	return microerror.Cause(err) == configMapKeyNotFoundError
 }
 
-var invalidConfigMapError = microerror.New("invalid config map")
+var invalidConfigMapError = &microerror.Error{
+	Kind: "invalidConfigMapError",
+}
 
 // IsInvalidConfigMap asserts invalidConfigMapError.
 func IsInvalidConfigMap(err error) bool {
 	return microerror.Cause(err) == invalidConfigMapError
 }
 
-var wrongNameError = microerror.New("wrong name")
+var wrongNameError = &microerror.Error{
+	Kind: "wrongNameError",
+}
 
 // IsWrongName asserts wrongNameError.
 func IsWrongName(err error) bool {
 	return microerror.Cause(err) == wrongNameError
 }
 
-var wrongNamespaceError = microerror.New("wrong namespace")
+var wrongNamespaceError = &microerror.Error{
+	Kind: "wrongNamespaceError",
+}
 
 // IsWrongNamespace asserts wrongNamespaceError.
 func IsWrongNamespace(err error) bool {
 	return microerror.Cause(err) == wrongNamespaceError
 }
 
-var wrongTypeError = microerror.New("wrong type")
+var wrongTypeError = &microerror.Error{
+	Kind: "wrongTypeError",
+}
 
 // IsWrongTypeError asserts wrongTypeError.
 func IsWrongTypeError(err error) bool {
