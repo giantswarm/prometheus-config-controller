a0c0b1d39860a88a8ee9a32d5146bd063b7e719e
Paweł Kopiczko <kopiczko@gmail.com>
2020-01-17 13:15:15 +0000

    resource/configmap: simplify resource

--- service/controller/v1/resource/configmap/configmap.go
+++ service/controller/v1/resource/configmap/configmap.go
@@ -1,8 +1,13 @@
 package configmap
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/controller/context/finalizerskeptcontext"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/giantswarm/prometheus-config-controller/service/controller/v1/prometheus"
@@ -75,4 +80,64 @@ func New(config Config) (*Resource, error) {
 
 func (r *Resource) Name() string {
 	return Name
+}
+
+func (r *Resource) ensure(ctx context.Context, obj interface{}) error {
+	currentCM, err := r.getCurrentState(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	desiredCM, err := r.getDesiredState(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	cm := newConfigMapToUpdate(currentCM, desiredCM)
+
+	{
+		// currentCM is used in the log messages because cm can be nil.
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("updating ConfigMap %#q in namespace %#q", currentCM.GetName(), currentCM.GetNamespace()))
+
+		if cm == nil {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ConfigMap %#q in namespace %#q is up to date", currentCM.GetName(), currentCM.GetNamespace()))
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", "cancelling resource")
+			return nil
+		}
+
+		r.k8sClient.CoreV1().ConfigMaps(cm.GetNamespace()).Update(cm)
+		if apierrors.IsNotFound(err) {
+			r.k8sClient.CoreV1().ConfigMaps(cm.GetNamespace()).Create(cm)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		} else if err != nil {
+			return microerror.Mask(err)
+
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("updated ConfigMap %#q in namespace %#q", currentCM.GetName(), currentCM.GetNamespace()))
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "reloading prometheus configuration")
+
+		// We attempt to reload Prometheus even if the configmap hasn't updated,
+		// as the PrometheusReloader takes care that we don't reload too often.
+		err := r.prometheusReloader.Reload(ctx)
+		if prometheus.IsReloadThrottle(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "did not reload prometheus configuration")
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", err.Error())
+			r.logger.LogCtx(ctx, "level", "debug", "message", "keeping finalizers")
+			finalizerskeptcontext.SetKept(ctx)
+		} else if err != nil {
+			return microerror.Mask(err)
+		} else {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "reloaded prometheus configuration")
+		}
+	}
+
+	return nil
 }
